9cc539dbe90ed436fbcadb75b02f3a2ddcacee55
rogerioguicampos <rogerio.guicampos@gmail.com>
2020-04-11 22:49:34 -0300

    Vis docstring

--- 2020_04_06_Quantile_Regression/code/2_LossOutlierVis.py
+++ 2020_04_06_Quantile_Regression/code/2_LossOutlierVis.py
@@ -1,5 +1,11 @@
 """
+Treinamos o Gradient Boosting Regressor com as 
+loos ls, lad e huber nos dados com outliers sinteticos.
 
+Colocamos uma funcao para criar um scatter plot, assim
+podemos ver como cada loss se comporta nos dados de treino e teste.
+Aqui plotamos somente a loss ls, sinta-se livre para plotar o que quiser
+com a funcao plot_scatter().
 """
 # importando bibliotecas
 import pandas as pd
@@ -7,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.ensemble import GradientBoostingRegressor
 import matplotlib.pyplot as plt
+
 
 # funcoes auxiliares
 def plot_scatter(X: pd.Series, y_1: pd.Series, y_2: pd.Series, 
@@ -16,7 +23,7 @@ def plot_scatter(X: pd.Series, y_1: pd.Series, y_2: pd.Series,
     pd.Series y diferentes
     
     Arguments:
-        X {pd.Series} -- [description]
+        X {pd.Series} -- valores de x para plotar
         y_1 {pd.Series} -- primeiro valor de y para plotar
         y_2 {pd.Series} -- segundo valor de y para plotar
         label_1 {str} -- label de y_1
@@ -25,7 +32,7 @@ def plot_scatter(X: pd.Series, y_1: pd.Series, y_2: pd.Series,
         yscale {str} -- escala de y={'linear', 'log', 'symlog', 'logit'}
     """
     plt.scatter(X, y_1, label=label_1)
-    plt.scatter(X,y_2, label=label_2)
+    plt.scatter(X,y_2, label=label_2, alpha=0.6)
     plt.title(title)
     plt.yscale(value=yscale)
     plt.legend()
@@ -68,16 +75,17 @@ df_train.loc[:, 'y_train'] = y_train_outlier
 df_test = pd.DataFrame()
 df_test.loc[:, 'y_test'] = y_test
 
-# treinando os modelos
+# treinando os modelos e inserindo previsoes dos dados 
+# de treino e de teste nos respectivos dataframes
 for i, model in enumerate([gbr_ls, gbr_lad, gbr_hub]):
     model.fit(X_train, y_train_outlier)
     df_train.loc[:, LIST_LOSS[i][0]] = model.predict(X_train)
     df_test.loc[:, LIST_LOSS[i][0]] = model.predict(X_test)
 
-# visualizacao dos dados de treino com outlier
+# visualizacao dos dados de treino com outlier com a loss ls
 plot_scatter(X=X_train.iloc[:, 0], y_1=y_train_outlier, y_2=df_train.loc[:, 'Least_Squares'], 
             label_1='Train_Real', label_2='Train_Pred', title='Dados de Treino', yscale='log')
 
-# visualizacao dos dados de teste
+# visualizacao dos dados de teste com a loss ls
 plot_scatter(X=X_test.iloc[:, 0], y_1=y_test, y_2=df_test.loc[:, 'Least_Squares'], 
             label_1='Test_Real', label_2='Test_Pred', title='Reais vs Previsão', yscale='log')
